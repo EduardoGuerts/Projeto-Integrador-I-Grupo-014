ec514242e3584635969f6544a173321f2aa6b2b0
Eduardo Batista Guerts <eduardoguerts@hotmail.com>
2024-04-17 22:42:28 -0300

    atualizado javascript
    adicionado janela javascript após envio com sucesso do formulário com botão para aluno consultar gabarito

--- js/script.js
+++ js/script.js
@@ -15,7 +15,41 @@ document.querySelector("form").addEventListener("submit", function (event) {
         event.stopPropagation();
         alert("Por favor, preencha todos os campos corretamente.");
     } else {
-        alert("Formulário enviado com sucesso!");
+        event.preventDefault();
+        event.stopPropagation();
+        // Cria uma nova div para a caixa de diálogo
+        var dialog = document.createElement("div");
+        dialog.style.width = "300px";
+        dialog.style.height = "200px";
+        dialog.style.position = "fixed";
+        dialog.style.top = "50%";
+        dialog.style.left = "50%";
+        dialog.style.marginTop = "-100px";
+        dialog.style.marginLeft = "-150px";
+        dialog.style.backgroundColor = "rgba(255, 255, 255, 0.8)"; // Adiciona transparência
+        dialog.style.padding = "20px";
+        dialog.style.boxShadow = "0px 0px 150px 3px rgba(0, 0, 0, 0.75)";
+        dialog.style.textAlign = "center"; // Alinha o texto ao centro
+        dialog.innerHTML = "<strong>Formulário enviado com sucesso!</strong>"; // Texto em negrito
+        
+        // Cria um novo botão
+        var btn = document.createElement("button");
+        btn.innerHTML = "Gabarito";
+        btn.style.backgroundColor = "green"; // Adiciona fundo verde
+        btn.style.color = "white"; // Adiciona cor de texto branca
+        btn.style.border = "none"; // Remove a borda
+        btn.style.padding = "10px 20px"; // Adiciona padding
+        btn.style.marginTop = "50px"; // Adiciona margem superior
+        btn.onclick = function () {
+            window.location.href = './pages/gabarito.html';
+        };
+        
+        // Adiciona o botão à caixa de diálogo
+        dialog.appendChild(btn);
+        
+        // Adiciona a caixa de diálogo ao corpo do documento
+        document.body.appendChild(dialog);
     }
     form.classList.add('was-validated');
 }, false);
+
